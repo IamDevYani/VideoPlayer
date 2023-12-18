c6b2dfc856181f475804e9373e232a0ef46470d3
IamDevYani <153744125+IamDevYani@users.noreply.github.com>
2023-12-18 22:16:18 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -12,4 +12,27 @@ let videoList = document.querySelectorAll('.video-list-container .list');
             document.querySelector('.main-video-container .main-video').play();
             document.querySelector('.main-video-container .main-vid-title').innerHTML = title;
         };
-        });
+});
+
+const url = 'https://api-football-v1.p.rapidapi.com/v3/timezone';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '7613a0d89cmsh173e7e69a982c5bp18fdf6jsn0388f5b82c80',
+		'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+	}
+};
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+fetch(url, options).then(function(response){
+    console.log('Success!', response);
+}).catch(function(err){
+    console.log('Something went worng')
+})
